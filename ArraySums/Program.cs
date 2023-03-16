18664f86800dc232fe80500533921e1817d6f812
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-03-16 22:20:23 +0000

    Recieve height and length and create an array of those proportions

--- ArraySums/Program.cs
+++ ArraySums/Program.cs
@@ -6,7 +6,18 @@ namespace ArraySums
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Give array height: ");
+            string height=Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write("Give array length: ");
+            string length=Console.ReadLine();
+            Console.WriteLine();
+
+            int h = int.Parse(height);
+            int l = int.Parse(length);
+
+            int[] matrix = new int [h , l];
         }
     }
 }
