37be3eebcc1321eace58b305fdc68dae4a4e218a
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-03-16 22:12:41 +0000

    Create project ArraySums

---
+++ ArraySums/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArraySums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
