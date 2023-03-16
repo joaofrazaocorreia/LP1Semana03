6f29d8b7327923336866f68dc7ff6974f6baf278
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-03-16 22:07:31 +0000

    Create project ChangeString. Recieve a string and a character, and print the string without the character.

---
+++ ChangeString/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string: ");
+            string givenString = Console.ReadLine();
+            Console.WriteLine("");
+
+            Console.WriteLine("Enter a character: ");
+            string character = Console.ReadLine();
+            Console.WriteLine("");
+            char givenChar = char.Parse(character);
+
+            string output="";
+
+            foreach (char c in givenString)
+            {
+                if (c != givenChar)
+                {
+                    output += c;
+                }
+            }
+
+            Console.WriteLine("Output: "+output);
+        }
+    }
+}
