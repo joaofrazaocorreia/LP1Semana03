5f61f447b0eb8720a61705ab3a407e7d61160fdb
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-03-16 23:16:21 +0000

    Create project PlayerPerks

---
+++ PlayerPerks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlayerPerks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
