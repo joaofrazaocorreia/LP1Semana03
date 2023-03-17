209cf9dea9a956def218c98437fb4c6efb3c3a4e
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-03-17 13:43:50 +0000

    Deactivate perks if they're even, then print the active ones.

--- PlayerPerks/Program.cs
+++ PlayerPerks/Program.cs
@@ -7,23 +7,23 @@ namespace PlayerPerks
         [Flags]
         enum perks
         {
-            WaterBreathing,
-            Stealth,
-            AutoHeal,
-            DoubleJump
+            WaterBreathing = 0,
+            Stealth = 0,
+            AutoHeal = 0,
+            DoubleJump = 0
         };
 
         static void Main(string[] args)
         {
+            perks myPerks = perks.WaterBreathing | perks.Stealth | perks.AutoHeal | perks.DoubleJump;
 
-            string input = Console.ReadLine();
             int w=0;
             int a=0;
             int s=0;
             int d=0;
             bool known=true;
 
-            foreach (char c in input)
+            foreach (char c in args[0])
             {
                 if (c==char.Parse("w"))
                 {
@@ -49,11 +49,64 @@ namespace PlayerPerks
                 {
                     known=false;
                 }
+            }
 
-                if (known)
+            if (known)
+            {
+                if (w==0 ^ w%2==0)
                 {
-                    
+                    myPerks ^= perks.WaterBreathing;
                 }
+
+                if (a==0 ^ a%2==0)
+                {
+                    myPerks ^= perks.AutoHeal;
+                }
+
+                if (s==0 ^ s%2==0)
+                {
+                    myPerks ^= perks.Stealth;
+                }
+
+                if (d==0 ^ d%2==0)
+                {
+                    myPerks ^= perks.DoubleJump;
+                }
+
+                int[] array= new int[4] {w,a,s,d};
+                foreach (int n in array)
+                {
+                    Console.WriteLine(n);
+                }
+
+
+                if (myPerks != 0)
+                {
+                    Console.WriteLine(myPerks);
+                }
+
+                else
+                {
+                    Console.WriteLine("No perks at all!");
+                } 
+
+                if ((myPerks & perks.Stealth) == perks.Stealth)
+                {
+                    if ((myPerks & perks.DoubleJump) == perks.DoubleJump)
+                    {
+                        Console.WriteLine("Silent Jumper!");
+                    }
+                }
+
+                if ((myPerks & perks.AutoHeal) != perks.AutoHeal)
+                {
+                    Console.WriteLine("Not gonna make it!");
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Unknown Perk!");
             }
         }
     }
