2996c2c144c99f9043eb1d28e841a39ef3733b93
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-03-17 13:09:05 +0000

    Detect the characters wasd in given string and count them

--- PlayerPerks/Program.cs
+++ PlayerPerks/Program.cs
@@ -4,9 +4,57 @@ namespace PlayerPerks
 {
     class Program
     {
+        [Flags]
+        enum perks
+        {
+            WaterBreathing,
+            Stealth,
+            AutoHeal,
+            DoubleJump
+        };
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            string input = Console.ReadLine();
+            int w=0;
+            int a=0;
+            int s=0;
+            int d=0;
+            bool known=true;
+
+            foreach (char c in input)
+            {
+                if (c==char.Parse("w"))
+                {
+                    w+=1;
+                }
+
+                else if (c==char.Parse("a"))
+                {
+                    a+=1;
+                }
+
+                else if (c==char.Parse("s"))
+                {
+                    s+=1;
+                }
+
+                else if (c==char.Parse("d"))
+                {
+                    d+=1;
+                }
+
+                else
+                {
+                    known=false;
+                }
+
+                if (known)
+                {
+                    
+                }
+            }
         }
     }
 }
